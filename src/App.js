666771c9b7281a67ddcd80bdaaef7ee869217c69
Kyle Rohlfing <krohlfing94@gmail.com>
2019-09-15 21:16:57 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import Pagination from './Pagination';
+import axios from 'axios';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextPageUrl, setNextPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [previousPageUrl, setPreviousPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    let CancelToken = axios.CancelToken;
+    let call1 = CancelToken.source();
+
+    axios.get(currentPageUrl, {
+      cancelToken: call1.token
+    }).then(res => {
+      setLoading(false);
+      setNextPageUrl(res.data.next);
+      setPreviousPageUrl(res.data.previous);
+      setPokemon(res.data.results.map(p => p.name));
+    });
+
+    return () => call1.cancel();
+  }, [currentPageUrl]);
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function gotoPreviousPage() {
+    setCurrentPageUrl(previousPageUrl);
+  }
+
+  if (loading) return "Loading...";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination gotoNextPage={nextPageUrl ? gotoNextPage : null} gotoPreviousPage={previousPageUrl ? gotoPreviousPage : null} />
+    </>
   );
 }
 
